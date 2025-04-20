aa9bc8314a1bfafb052745c9bc98e557243b0009
lazkopat3522 <ikramamzkg@gmail.com>
2025-04-20 12:49:40 -0400

    TwoSumLeet

---
+++ src/TwoSumLeet.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class TwoSumLeet {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] ar = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            ar[i]=sc.nextInt();}
+        int t=sc.nextInt();
+        int c=0;
+        while(c<n)
+        {
+            for(int j=0;j<n;j++) //2,7,11,15 target 9
+            {
+                if(ar[c]+ar[j]==t)
+                {
+                    System.out.print(c+" "+j);
+                    c=n-1;
+                }
+
+            }
+            c++;
+        }
+
+    }
+}
