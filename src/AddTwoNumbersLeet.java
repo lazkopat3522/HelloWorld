16d3a6101b1faec8656ed7747b9eb631d405946c
lazkopat3522 <ikramamzkg@gmail.com>
2025-04-23 23:50:45 -0400

    Add two numbers leet

---
+++ src/AddTwoNumbersLeet.java
@@ -0,0 +1,20 @@
+import java.sql.Array;
+import java.util.Scanner;
+
+public class AddTwoNumbersLeet {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int num1=0,num2=0;
+        int n1=sc.nextInt();//Vvodim kolichestvo elementov dlya pervogo lista
+        int[] arr1=new int[n1];
+        for(int i=0;i<n1;i++) {
+            arr1 = sc.nextInt();
+        }
+        int n2=sc.nextInt();//Vvodim kolichestvo elementov dlya vtorogo lista
+        int[] arr2=new int[n2];
+        for(int i=0;i<n2;i++) {
+            arr1 = sc.nextInt();
+
+        }
+    }
+}
