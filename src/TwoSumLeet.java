1ce9ae514d99d4eb476612aceacc6730a9ba8bab
lazkopat3522 <ikramamzkg@gmail.com>
2025-04-23 23:57:40 -0400

    Add two numbers leet2

--- src/TwoSumLeet.java
+++ src/TwoSumLeet.java
@@ -5,7 +5,6 @@ public class TwoSumLeet {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int[] ar = new int[n];
-
         for (int i = 0; i < n; i++) {
             ar[i]=sc.nextInt();}
         int t=sc.nextInt();
@@ -14,12 +13,10 @@ public class TwoSumLeet {
         {
             for(int j=0;j<n;j++) //2,7,11,15 target 9
             {
-                if(ar[c]+ar[j]==t)
-                {
-                    System.out.print(c+" "+j);
+                if(ar[c]+ar[j]==t) {
+                    System.out.println(c + " " + j);
                     c=n-1;
                 }
-
             }
             c++;
         }
