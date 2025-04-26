ee0ae9a0e9a98f9ca97e7fd96db16c6832151584
lazkopat3522 <ikramamzkg@gmail.com>
2025-04-25 23:46:16 -0400

    Reversed Integer Leetcode 2

--- src/ReversedIntLeet.java
+++ src/ReversedIntLeet.java
@@ -4,12 +4,18 @@ public class ReversedIntLeet {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
-        int num=0,c=0;
+        int revNum=0,c=0,newNum=n;
         while(n>0) // Understanding how many digits the number has
         {
             n=n/10; // Dividing by 10 each time while it is greater than 0
             c++;
         }
-        System.out.print("Number of digits = "+c);
+        while(c>0)
+        {
+            newNum=newNum%10;
+            revNum=revNum+(newNum*(int)Math.pow(10,c));
+            c--;
+        }
+        System.out.print(revNum);
     }
 }
