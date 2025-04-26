9e9dba4198df82c1f6a7e9cb89805f7361a8e818
lazkopat3522 <ikramamzkg@gmail.com>
2025-04-25 23:50:06 -0400

    Reversed Int Last version

--- src/ReversedIntLeet.java
+++ src/ReversedIntLeet.java
@@ -4,7 +4,7 @@ public class ReversedIntLeet {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
-        int revNum=0,c=0,newNum=n;
+        int revNum=0,c=0,newNum=n,dig=0;
         while(n>0) // Understanding how many digits the number has
         {
             n=n/10; // Dividing by 10 each time while it is greater than 0
@@ -12,8 +12,9 @@ public class ReversedIntLeet {
         }
         while(c>0)
         {
-            newNum=newNum%10;
-            revNum=revNum+(newNum*(int)Math.pow(10,c));
+            dig=newNum%10; // Substracting last digit
+            newNum=newNum/10;
+            revNum=revNum+(dig*(int)Math.pow(10,c-1)); // Multiplying digit by 10 to the power of number of digits -1
             c--;
         }
         System.out.print(revNum);
