29cc24bded4ca3e78f233711edf41179bb947371
lazkopat3522 <ikramamzkg@gmail.com>
2025-04-28 07:37:43 -0400

    methods

---
+++ src/Exercise.java
@@ -0,0 +1,13 @@
+public class Exercise {
+    public static void main(String[] args) {
+
+        calculateScore(false,500);
+    }
+    public static void calculateScore(boolean gameOver,int b){
+        if(gameOver) {
+            System.out.println(b+b);
+        } else {
+            System.out.println(b * b);
+        }
+    }
+}
