b876b0312429474a3844486ac85d7ad77a1d3e76
lazkopat3522 <ikramamzkg@gmail.com>
2025-04-28 07:40:24 -0400

    methods2

--- src/Exercise.java
+++ src/Exercise.java
@@ -1,7 +1,8 @@
 public class Exercise {
     public static void main(String[] args) {
 
-        calculateScore(false,500);
+        calculateScore(false,5);
+        calculateScore(true,5);
     }
     public static void calculateScore(boolean gameOver,int b){
         if(gameOver) {
