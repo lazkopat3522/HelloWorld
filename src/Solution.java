51add0825ab26ae6cb7c0a02487b6699ce8fecf4
lazkopat3522 <ikramamzkg@gmail.com>
2025-04-30 00:05:01 -0400

    findEven

--- src/Solution.java
+++ src/Solution.java
@@ -1,7 +1,22 @@
 public class Solution {
+    public static void main(String[] args) {
+        findNumbers(2,123,2134,4545,123334);
+    }
     public int findNumbers(int[] nums) {
-    for(int i=0;i<nums.length;i++) {
-
+        int num=0,c=0,cEven=0;
+        for(int i=0;i<nums.length;i++) {
+        num=nums[i];
+        while(num>0){
+            num=num/10;
+            c++;
+            }
+        if(c%2==0){
+            cEven++;
         }
+        c=0;
+        }
+        if(cEven!=0){
+        return cEven;
+        } else return -1;
     }
 }
