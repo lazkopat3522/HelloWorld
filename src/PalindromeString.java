a669a26e90c26236ad27c654dbf3eec92d9ba9cc
lazkopat3522 <ikramamzkg@gmail.com>
2025-05-01 14:28:21 -0400

    Palindrome String

--- src/PalindromeString.java
+++ src/PalindromeString.java
@@ -3,12 +3,15 @@ import java.util.Scanner;
 public class PalindromeString {
     public static void main(String[] args) {
         String rev="";
-        Scanner sc=new Scanner(System.in);
-        String st=sc.nextLine();
+        Scanner s=new Scanner(System.in);
+        String st=s.nextLine();
         for(int i=st.length()-1;i>=0;i--){
             rev=rev+st.charAt(i);
         }
-        System.out.println(st+"<->"+rev);
+        if(st.equals(rev)){
+            System.out.println("Number is PALINDROME");
+        } else System.out.println("Number is NOT PALINDROME");
+
     }
 
 }
