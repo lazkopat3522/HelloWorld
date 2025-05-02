c103eff7069e66f3a709466838e363d9cdf4649a
lazkopat3522 <ikramamzkg@gmail.com>
2025-05-02 08:37:47 -0400

    String Method

---
+++ src/Method.java
@@ -0,0 +1,8 @@
+public class Method {
+    public static void greet(String name) {
+        System.out.println("Hello "+name+" !");
+    }
+    public static void main(String[] args) {
+        greet("Ikram");
+    }
+}
