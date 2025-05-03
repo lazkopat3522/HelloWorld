26bbae54b58c76cffc3324afadc14e1c635b476a
lazkopat3522 <ikramamzkg@gmail.com>
2025-05-02 22:36:16 -0400

    manipulation

--- src/Method.java
+++ src/Method.java
@@ -4,11 +4,18 @@ public class Method {
         public static int square(int a) {
             return a*a;
         }
+
+    public static int sum(int a,int b) {
+            return a+b;
+
+    }
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int a=sc.nextInt();
-
-            System.out.println(square(a));
+        int b=sc.nextInt();
+        System.out.println("This is the sum of two numbers method => "+sum(a,b));
+        System.out.println("This is the square of "+b+" -> "+square(b));
+            System.out.println("This is the square of "+a+" -> "+square(a));
     }
 
 
