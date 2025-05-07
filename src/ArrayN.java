59219f71dc39fef13ca53c888049ff5f723c0943
lazkopat3522 <ikramamzkg@gmail.com>
2025-05-07 01:01:13 -0400

    Array N problem Informatics

--- src/ArrayN.java
+++ src/ArrayN.java
@@ -11,11 +11,18 @@ public class ArrayN {
         int step=sc.nextInt();
         if(step>0) {
             for (int i = 0; i < n; i++) {
-                int index=((i+step)%n)+n;
+                int index=((i+step)%n)+n;//finding the new index and plus n  is to place in new index after n+
                 arr[index]=arr[i];
                 }
-            for(int i=n;i<n+n;i++)
+            for(int i=n;i<n+n;i++) //same array but starting from n+1 element where i have sorted with n amount of steps
                 System.out.print(arr[i]+" ");
+            } else {
+            for (int i = 0; i < n; i++) {
+                int index=(n-(Math.abs((i-Math.abs(step)))%n))+n;//finding the new index and plus n  is to place in new index after n+
+                arr[index]=arr[i];
             }
+            for(int i=n;i<n+n;i++) //same array but starting from n+1 element where i have sorted with n amount of steps
+                System.out.print(arr[i]+" ");
+        }
         }
     }
