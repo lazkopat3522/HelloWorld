583dcec6cab7e80442ee428ab20b05149a524435
lazkopat3522 <ikramamzkg@gmail.com>
2025-05-07 00:13:27 -0400

    Array N problem Informatics

--- src/ArrayN.java
+++ src/ArrayN.java
@@ -4,28 +4,18 @@ public class ArrayN {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
-        int dp1=0,dp2=0;
-        int[] arr=new int[n];
-        boolean[] barr=new boolean[n]
+        int[] arr=new int[n+n];
         for(int i=0;i<n;i++){
             arr[i]=sc.nextInt();
-            barr[i]=true;
         }
         int step=sc.nextInt();
         if(step>0) {
-            for (int i = 0; i < n; i++) {
-                if(barr[i]==true){
-                    int index=(i+step)%n;
-                    int newIndex=(index+step)%n;
-                    dp1=arr[index];
-                    dp2=arr[newIndex];
-                    arr[index]=arr[i];
-                    arr[newIndex]=dp1;
-                    barr[index]=false;
-                    barr[newIndex]=false;
+            for (int i = 0; i < n+n; i++) {
+                int index=((i+step)%n)+n;
+                arr[index]=arr[i];
                 }
-                }
-
+            for(int i=n;i<n+n;i++)
+                System.out.println(arr[i]+" ");
             }
         }
     }
