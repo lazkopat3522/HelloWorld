26f6761532e801975d0b4152c31bbd6e5b6d8f5d
lazkopat3522 <ikramamzkg@gmail.com>
2025-05-11 23:40:25 -0400

    methods in Java informatics

--- src/InformaticsFunctionsA.java
+++ src/InformaticsFunctionsA.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 
 public class InformaticsFunctionsA {
+
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int a=sc.nextInt();
@@ -11,16 +12,19 @@ public class InformaticsFunctionsA {
         System.out.println(minOfNumbers(a,b,c,d));
 
     }
-    public static int minOfNumbers(int a,int b,int c,int d){
-     if(a<b && a<c && a<d){
-         return a;
-     } else if(b<a && b<c && b<d){
-         return b;
-     } else if(c<a && c<b && c<d){
-         return c;
-     } else if(d<a && d<b && d<c){
-         return d;
-     }
+
+    private static int minOfNumbers(int a,int b,int c,int d){
+        int min=0;
+        if(a<b && a<c && a<d){
+            min=a;
+        } else if(b<a && b<c && b<d){
+            min=b;
+        } else if(c<a && c<b && c<d){
+            min=c;
+        } else if(d<a && d<b && d<c) {
+            min=d;
+        }
+        return min;
     }
 
 }
