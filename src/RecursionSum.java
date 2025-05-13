60633b5ee67ac005d5719521fc7491ac0e40581c
lazkopat3522 <ikramamzkg@gmail.com>
2025-05-12 23:47:30 -0400

    Recursion sum of numbers

---
+++ src/RecursionSum.java
@@ -0,0 +1,10 @@
+public class RecursionSum {
+    public static void main(String[] args) {
+
+    }
+    public static int sumOfN(int n){
+        if(n<1){
+            return n;
+        } return n+(n-1);
+    }
+}
