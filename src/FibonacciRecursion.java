b376eb3aa7d5d4e4e2ade596741a06de95782e3b
lazkopat3522 <ikramamzkg@gmail.com>
2025-05-12 23:54:29 -0400

    Fibonacci Recursion

---
+++ src/FibonacciRecursion.java
@@ -0,0 +1,10 @@
+public class FibonacciRecursion {
+    public static void main(String[] args) {
+        System.out.println(fibonacci(6));
+    }
+    public static int fibonacci(int n){
+        if(n<=1){
+            return n;
+        } return (fibonacci(n-1)+fibonacci(n-2));
+    }
+}
