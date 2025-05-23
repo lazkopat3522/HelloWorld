0e5520efffc3e806e36899cb45889363ffc45c26
lazkopat3522 <ikramamzkg@gmail.com>
2025-05-22 19:31:37 -0700

    Boolean

--- Boolean.java
+++ Boolean.java
@@ -3,11 +3,12 @@ import java.util.Scanner;
 public class Boolean {
     public static void main(String[] args) {
     Scanner sc=new Scanner(System.in);
-    String car=sc.nextLine();
-    boolean boo=car=="Toyota"?false:true;
+    String car="Honda";
+    boolean boo = car == "Honda" ? false:true;
     if(boo==false){
         System.out.println("You entered wrong make");
     } else System.out.println("Congrats");
+        System.out.println("The value that boo gets is >>> "+boo);
     }
 
 }
