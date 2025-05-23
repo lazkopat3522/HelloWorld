90e72c465c5086c8125bc950352cbccaac344327
lazkopat3522 <ikramamzkg@gmail.com>
2025-05-22 19:22:32 -0700

    Boolean

--- Boolean.java
+++ Boolean.java
@@ -1,9 +1,13 @@
+import java.util.Scanner;
+
 public class Boolean {
     public static void main(String[] args) {
-    boolean f=true;
-    if(f){
-        System.out.println("Yes this is true");
-    }
+    Scanner sc=new Scanner(System.in);
+    String car=sc.nextLine();
+    boolean boo=car=="Toyota"?false:true;
+    if(boo==false){
+        System.out.println("You entered wrong make");
+    } else System.out.println("Congrats");
     }
 
 }
