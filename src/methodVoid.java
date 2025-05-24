cd78244957372540b785e7bfd77b0ae47b2449e0
lazkopat3522 <ikramamzkg@gmail.com>
2025-05-23 22:41:43 -0700

    Ignoring Cases

--- src/methodVoid.java
+++ src/methodVoid.java
@@ -9,7 +9,7 @@ public class methodVoid {
         callMeBaby(str);
     }
     public static void callMeBaby(String name){
-        boolean boo=(name.equals("Ikram"))?true:false;
+        boolean boo=(name.equalsIgnoreCase("ikram"))?true:false;
         if(boo){
             System.out.println("Hi "+name);
         } else System.out.println("I dont know you to greet");
