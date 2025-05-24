ec47a279786213dc5856806f500e9b49ad0f798d
lazkopat3522 <ikramamzkg@gmail.com>
2025-05-23 22:39:46 -0700

    method greet

--- src/methodVoid.java
+++ src/methodVoid.java
@@ -6,11 +6,10 @@ public class methodVoid {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         String str=sc.next();
-        String newString=str;
-        callMeBaby(newString);
+        callMeBaby(str);
     }
     public static void callMeBaby(String name){
-        boolean boo=(name=="usa")?true:false;
+        boolean boo=(name.equals("Ikram"))?true:false;
         if(boo){
             System.out.println("Hi "+name);
         } else System.out.println("I dont know you to greet");
