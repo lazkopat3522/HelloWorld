47362a13917a97b9c91068caf5b966b82161c30f
lazkopat3522 <ikramamzkg@gmail.com>
2025-05-24 20:46:05 -0700

    minimum by function

--- src/minFunction.java
+++ src/minFunction.java
@@ -5,20 +5,14 @@ import java.util.Scanner;
 public class minFunction {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        int a= sc.nextInt();
-        int b= sc.nextInt();
-        int c= sc.nextInt();
-        int d= sc.nextInt();
+        boolean a= sc.nextBoolean();
+        int b= sc.nextBoolean();
 
-        System.out.println(min(a,b,c,d));
+
     }
-    public static int min(int a,int b,int c,int d){
-        if(a<b && a<c && a<d){
-            return a;
-        } else if (b<a && b<c && b<d) {
-          return b;
-        } else if (c<a && c<b && c<d) {
-            return c;
-        } else return d;
+    public static boolean xor(boolean a,boolean b){
+        if ((a==true || b==true)||){
+
+        }
     }
 }
