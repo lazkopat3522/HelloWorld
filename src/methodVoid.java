b9b1368aa50578ba5200283ed014a80f0f99ea58
lazkopat3522 <ikramamzkg@gmail.com>
2025-05-26 23:07:30 -0700

    two methods

--- src/methodVoid.java
+++ src/methodVoid.java
@@ -6,13 +6,25 @@ public class methodVoid {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         String str=sc.next();
+        int p=sc.nextInt();
         callMeBaby(str);
+        System.out.println(" you got "+calculateExamScore(p)+" on exam");
     }
     public static void callMeBaby(String name){
         boolean boo=(name.equalsIgnoreCase("ikram"))?true:false;
         if(boo){
-            System.out.println("Hi "+name);
-        } else System.out.println("I dont know you to greet");
+            System.out.print("Hi "+name);
+        } else System.out.print("I dont know you to greet");
 
+    }
+    public static int calculateExamScore(int point){
+        if(point>=80){
+            return 5;
+        } else if(point<80 && point>=60){
+            return 4;
+
+        } else if (point<60 && point>=40) {
+            return 3;
+        } else return 2;
     }
 }
