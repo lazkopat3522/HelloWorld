aeb974794bf3d3b1c0d12593ddeca993d81037cf
lazkopat3522 <ikramamzkg@gmail.com>
2025-05-28 23:09:47 -0700

    Shariki

--- src/Informatics/ArrayO.java
+++ src/Informatics/ArrayO.java
@@ -1,16 +1,18 @@
 package src.Informatics;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class ArrayO {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        int n= sc.nextInt();
-        int[] arr=new int[n];
-        int c=0;
-        for(int i=0;i<n;i++){
-            arr[i]= sc.nextInt();
-        }
+        List<Integer> arr = new ArrayList<>();
+        arr.add(1);
+        arr.add(2);
+        arr.add(4);
 
+        for(int i=0;i<arr.size();i++){
+            System.out.println(arr.get(i)+" ");
+        }
     }
 }
