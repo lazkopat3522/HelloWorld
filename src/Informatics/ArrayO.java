cfc5036c2e0bb3368b674e90191445deab170ccb
lazkopat3522 <ikramamzkg@gmail.com>
2025-05-28 22:50:26 -0700

    Shariki

---
+++ src/Informatics/ArrayO.java
@@ -0,0 +1,16 @@
+package src.Informatics;
+
+import java.util.Scanner;
+
+public class ArrayO {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n= sc.nextInt();
+        int[] arr=new int[n];
+        int c=0;
+        for(int i=0;i<n;i++){
+            arr[i]= sc.nextInt();
+        }
+
+    }
+}
