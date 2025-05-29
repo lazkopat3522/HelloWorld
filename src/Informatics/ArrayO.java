5fcfc59754c1526124808ad6711680ff572d74c9
lazkopat3522 <ikramamzkg@gmail.com>
2025-05-28 23:36:15 -0700

    Shariki

--- src/Informatics/ArrayO.java
+++ src/Informatics/ArrayO.java
@@ -7,6 +7,7 @@ import java.util.Scanner;
 public class ArrayO {
     public static void main(String[] args) {
         List<Integer> arr = new ArrayList<>();
+        boolean b=false;
         arr.add(1);
         arr.add(3);
         arr.add(3);
@@ -14,19 +15,25 @@ public class ArrayO {
         arr.add(1);
         arr.add(1);
         arr.add(2);
-        int c = 0;
-        for (int i = 0; i < arr.size(); i++) {
-            if (arr.get(i) == arr.get(i + 1) && arr.get(i) == arr.get(i - 1)) {
-                arr.remove(i);
-                arr.remove(i + 1);
-                arr.remove(i - 1);
+        int c=0,exb=0,d=0;
+        boolean b1=false;
+        while(b==false){
+          if(arr.get(c)==arr.get(c+1)){
+              while(b1){
+                  if(arr.get(c)==arr.get(d+1)){
+                      exb++;
 
-            }
+                  } else b1=true;
+                  d++;
+              }
+              if(exb>=3){
+                  for(int i=c;i<c+exb;i++){
+                      arr.remove(i);
+                  }
+              }
+          }
+        c++;
         }
-        for (int i = 0; i < arr.size(); i++) {
-            System.out.println(arr.get(i) + " ");
-        }
-
     }
-    }
+}
 
