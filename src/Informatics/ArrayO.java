6a012ee84226b50695ce68bbf96192ca54bc3a7c
lazkopat3522 <ikramamzkg@gmail.com>
2025-05-28 23:19:00 -0700

    Shariki

--- src/Informatics/ArrayO.java
+++ src/Informatics/ArrayO.java
@@ -8,11 +8,25 @@ public class ArrayO {
     public static void main(String[] args) {
         List<Integer> arr = new ArrayList<>();
         arr.add(1);
+        arr.add(3);
+        arr.add(3);
+        arr.add(3);
+        arr.add(1);
+        arr.add(1);
         arr.add(2);
-        arr.add(4);
+        int c = 0;
+        for (int i = 0; i < arr.size(); i++) {
+            if (arr.get(i) == arr.get(i + 1) && arr.get(i) == arr.get(i - 1)) {
+                arr.remove(i);
+                arr.remove(i + 1);
+                arr.remove(i - 1);
 
-        for(int i=0;i<arr.size();i++){
-            System.out.println(arr.get(i)+" ");
+            }
         }
+        for (int i = 0; i < arr.size(); i++) {
+            System.out.println(arr.get(i) + " ");
+        }
+
     }
-}
+    }
+
