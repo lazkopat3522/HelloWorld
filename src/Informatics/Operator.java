58de1b1a6212862e67762c4943935ac84d3cfec3
lazkopat3522 <ikramamzkg@gmail.com>
2025-05-30 22:34:14 -0700

    if check

--- src/Informatics/Operator.java
+++ src/Informatics/Operator.java
@@ -1,8 +1,12 @@
 package src.Informatics;
 
+import java.util.Scanner;
+
 public class Operator {
     public static void main(String[] args) {
-        checkNumber(-12);
+        Scanner sc=new Scanner(System.in);
+        int n= sc.nextInt();
+        checkNumber(n);
     }
     public static void checkNumber(int num){
         System.out.println((num>0)?"positive":((num<0)?"negative":"zero"));
