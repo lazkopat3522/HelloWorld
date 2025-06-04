5be25e90ae78df1a7b07391eef7751d57f8d7af1
lazkopat3522 <ikramamzkg@gmail.com>
2025-06-03 23:32:40 -0700

    shariki array

---
+++ src/Informatics/milesToKm.java
@@ -0,0 +1,12 @@
+package src.Informatics;
+
+public class milesToKm {
+    public static double milesTokilometr(int miles){
+        return miles*1.67;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(milesTokilometr(1000));
+    }
+
+}
