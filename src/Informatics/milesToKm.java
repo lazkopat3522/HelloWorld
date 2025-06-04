13ff492759d4670a24e534b0f9fe01ec88e1b8b7
lazkopat3522 <ikramamzkg@gmail.com>
2025-06-03 23:33:43 -0700

    miles to km

--- src/Informatics/milesToKm.java
+++ src/Informatics/milesToKm.java
@@ -1,12 +1,16 @@
 package src.Informatics;
 
+import java.util.Scanner;
+
 public class milesToKm {
     public static double milesTokilometr(int miles){
         return miles*1.67;
     }
 
     public static void main(String[] args) {
-        System.out.println(milesTokilometr(1000));
+        Scanner sc=new Scanner(System.in);
+        int m=sc.nextInt();
+        System.out.println(milesTokilometr(m));
     }
 
 }
