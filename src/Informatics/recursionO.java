f2171968feb4da09de80e5e79fcd82079dcbc48e
lazkopat3522 <ikramamzkg@gmail.com>
2025-06-04 23:35:08 -0700

    prime check

---
+++ src/Informatics/recursionO.java
@@ -0,0 +1,22 @@
+package src.Informatics;
+
+import java.util.Scanner;
+
+public class recursionO {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        System.out.println(primeCheck(n));
+    }
+    public static String primeCheck(int number){
+        int c=0;
+        for(int i=1;i<=number/2;i++){
+            if(number%i==0){
+                c++;
+            }
+            if(c==1){
+                return "It is prime";
+            } else return "It is NOT prime";
+        } return "";
+    }
+}
