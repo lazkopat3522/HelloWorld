db6af314f356f5a0f974f2e2ffb88e73b37349c5
lazkopat3522 <ikramamzkg@gmail.com>
2025-06-06 23:55:26 -0700

    bit combinations

--- bitString.java
+++ bitString.java
@@ -1,6 +1,13 @@
 public class bitString {
     public static void main(String[] args) {
-
+        System.out.println(giveMeFive(0));
+    }
+    static String giveMeFive(int a){
+        if(a>0){
+            return "Salam aleykum" ;
+        } else if(a<0){
+            return "Chachyndy tara";
+        } else return "Nol okshoyt";
     }
 
 }
