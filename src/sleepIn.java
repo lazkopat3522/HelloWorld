e92e5364d93c6695ef2a73669e59eb7c2954581d
lazkopat3522 <ikramamzkg@gmail.com>
2025-06-11 23:55:27 -0700

    coding bat

--- src/sleepIn.java
+++ src/sleepIn.java
@@ -2,7 +2,9 @@ package src;
 
 public class sleepIn {
     public static void main(String[] args) {
-        System.out.println(sleepOn(true,false));
+        if(sleepOn(false,false)==true){
+            System.out.println("You can sleep till to 1 pm");
+        } else System.out.println("It is time to work");
     }
     public static boolean sleepOn(boolean weekday,boolean vacation ){
         if(!weekday || vacation){
