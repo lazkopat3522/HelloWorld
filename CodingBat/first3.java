f56aaf4ef05ff680a46fbcabf16ad3c2efed1e5b
lazkopat3522 <ikramamzkg@gmail.com>
2025-06-14 23:56:03 -0700

    first three

--- CodingBat/first3.java
+++ CodingBat/first3.java
@@ -2,7 +2,7 @@ package CodingBat;
 
 public class first3 {
     public static String stringFirstThree(String str,int n){
-        if(n<=1){
+        if(n<1){
             return str.substring(3); // when you write without giving index it is automatically start from 0
         } else {
             return str.substring(3)+stringFirstThree(str,n-1);
