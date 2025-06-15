f8806ec1a78b52903a18af7648d7e56bf5f5a1cd
lazkopat3522 <ikramamzkg@gmail.com>
2025-06-14 23:39:30 -0700

    first three

--- CodingBat/first3.java
+++ CodingBat/first3.java
@@ -3,12 +3,12 @@ package CodingBat;
 public class first3 {
     public static String stringFirstThree(String str,int n){
         if(n<=1){
-            return str.substring(0,3);
+            return str.substring(0,6);
         } else {
-            return str.substring(0,3)+stringFirstThree(str,n-1);
+            return str.substring(0,6)+stringFirstThree(str,n-1);
         }
     }
     public static void main(String[] args) {
-        System.out.println(stringFirstThree("Chocolate",4));
+        System.out.println(stringFirstThree("Kyrgyzstan",10));
     }
 }
