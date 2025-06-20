f1c3bed61eeba6da299250e74bc98252fe6715a5
lazkopat3522 <ikramamzkg@gmail.com>
2025-06-19 23:44:26 -0700

    Remove Duplicates from string

--- src/charH.java
+++ src/charH.java
@@ -1,10 +1,12 @@
 package src;
 
 import java.util.HashMap;
+import java.util.Scanner;
 
 public class charH {
     public static void main(String[] args) {
-        String str = "hello";
+        Scanner sc=new Scanner(System.in);
+        String hellstr=sc.nextLine();
         HashMap<Character, Integer> map = new HashMap<>();
         for (char c : str.toCharArray()){
             map.put(c,map.getOrDefault(c,0)+1);
