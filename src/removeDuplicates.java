0d5dcd0821d1308b0c3186739afe81aeb34ee87c
lazkopat3522 <ikramamzkg@gmail.com>
2025-06-19 23:41:16 -0700

    Remove Duplicates from string

--- src/removeDuplicates.java
+++ src/removeDuplicates.java
@@ -3,14 +3,14 @@ package src;
 public class removeDuplicates {
     public static void main(String[] args) {
         String str="arratatwdwdd";
-        String res="";
+        String strNew="";
         for(int i=0;i<str.length();i++){
-            if(!res.contains(String.valueOf(str.charAt(i)))){
-                res+=str.charAt(i);
+            if(!strNew.contains(String.valueOf(str.charAt(i)))){
+                strNew+=str.charAt(i);
             } else {
                 System.out.println("Duplicate found: "+str.charAt(i));
             }
         }
-        System.out.println(res);
+        System.out.println(strNew);
     }
 }
